fddb470457b81a46170df2e7ae4cbbb29a711adb
bline1990 <mloki1989@gmail.com>
2022-05-05 15:02:03 +0200

    prpmjena API key

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = '975e7107-ca61-485b-9c32-d1ad50804597';
 export const MODAL_CLOSE_SEC = 2.5;
