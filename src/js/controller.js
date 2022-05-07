4ddfaaf6f0978a04107622461f366f42f42593b2
bline1990 <mloki1989@gmail.com>
2022-05-07 10:20:54 +0200

    izmjena

--- src/js/controller.js
+++ src/js/controller.js
@@ -52,7 +52,7 @@ const controlSearchResults = async function () {
     await model.loadSearchResults(query);
 
     // 3) Render results
-    resultsView.render(model.getSearchResultsPage());
+    resultsView.render(model.getSearchResultsPage()); // pokaziva dio sa strane sa rezultatima
 
     // 4) Render initial pagination buttons
     paginationView.render(model.state.search);
